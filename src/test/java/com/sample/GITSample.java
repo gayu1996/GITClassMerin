a30024b05152419d3e8d19dda3fb71c9356d1a5d
gayu1996 <gayu1996.r@gmail.com>
2024-05-23 13:01:42 +0530

    First time push

---
+++ src/test/java/com/sample/GITSample.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class GITSample {
+
+	private void add() {
+		System.out.println("Addition");
+	}
+
+	private void sub() {
+		System.out.println("Subtraction");
+	}
+
+}
